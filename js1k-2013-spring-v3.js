520bd895640aa177d51bb594a13024d7639a5022
Benjamin Planche <benjamin.planche@aldream.net>
2013-02-27 17:13:24 +0900

    <JS1K> 889bits! Time to add stuff!

--- js1k-2013-spring-v3.js
+++ js1k-2013-spring-v3.js
@@ -13,92 +13,68 @@
 			document.body.clientWidth; // fix bug in webkit: http://qfox.nl/weblog/218
 		</script>
 		<script>
-var landscapes=[], waterHeight=6, heightSnow=10,heightSnowLimit=9.5,heightRock=9,heightMeadow=7.2,heightBeach=7.1; 
-color = function(h,s,l) {
-	p='%';
-	return 'hsl('+[18*h,9*s+'%',15*l+.5|0]+'%)';
-}
+d=[], h=e=6, b = function(h,s,l) { return 'hsl('+[94*h,15*s+'%',60*l]+'%)'; }
 		
 // Generating the landscape with various levels of details :
-for(it=10;it-=2;){
-	imIni=[0,9,coef=5,3];// Initial description of our map : a hole, a bump, and some stuff between ...
-	rowsIni=2;
-	for (l=1; l <=it; l++){
-		var rows = rowsIni*2-1;
-		var im = [];
-		coef /= 2;
-		for (var i=0; i<rows; i++){
-			for (var j=0; j<rows; j++){
-				var m=i/2|0, n=j/2|0, u=m*rowsIni+n, v=imIni[u], w=i*rows+j;
-				m = im[w] = ((i%2)?
-					(j%2)? ((im[w-rows]+im[w-1]+imIni[u+rowsIni+1])/3):((v+imIni[u+rowsIni])/2):
-					(j%2)? ((v+imIni[u+1])/2):v)+Math.random()*coef;
-				if (l==it && m < waterHeight)
-					im[w] = waterHeight;
+for(f=10;f-=2;){
+	g=[0,9,h,o=2];// Initial description of our map : a hole, a bump, and some stuff between ...
+	for (l=1; l <=f; l++){
+		p = o*2-1,q = [],h /= 2
+		for (i=p; i--;){
+			for (j=p; j--;){
+				v=g[u=o*(i/2|0)+j/2|0], w=i*p+j, q[w] = ((i%2)?
+					(j%2)? ((q[w+p]+q[w+1]+v)/3):((v+g[u+o])/2):
+					(j%2)? ((v+g[u+1])/2):v)+h*Math.random();
+				if (l==f && q[w] < e)
+					q[w] = e
 			}
 		}
-		rowsIni=rows; imIni=im;
+		o=p, g=q
 	}
-	var step = 20/Math.pow(2, 8);
-	var n = 0;landscapes[it] = [];
-	for (i = 0; i < rowsIni-1; i++) {
-		for (j = 0; j < rowsIni-1; j++) {
-			v=i*rowsIni+j;w=imIni[v];
+	r = 20/Math.pow(2, 8),n = 0,d[f] = [],p=o-1
+	for (i = p; i--;) {
+		for (j = p; j--;) {
+			v=g[u=o*i+j]
 			for (k=0;k<2;k++){
-				landscapes[it][n++] = [
-					i*step,
-					j*step,
-					w,
-					(i+1-n%2)*step,
-					(j+n%2)*step,
-					z=imIni[v+(n%2?1:rowsIni)],
-					(i+1)*step,
-					(j+1)*step,
-					y=imIni[v+rowsIni+1],
-					x=1-(z-y)/step,
-					z>heightSnow? color(0,0,x*9):
-						z>heightSnowLimit? Math.random()>.6? color(0,0,x*9):color(3,2,x*2):
-							z>heightRock? x>.8? color(7,4,x*2):color(3,1,x*2):
-								z>heightMeadow?  x>.5? color(7,7,x*2):color(7,4,x*2):
-									z>heightBeach? color(3,8,x*4):
-										Math.random()>.6? color(10,7,5):color(10,6,5)];
+				d[f][n++] = [
+					m=i*r,
+					q=j*r,
+					v,
+					(i+1-n%2)*r,
+					(j+n%2)*r,
+					z=g[u+(n%2?1:o)],
+					m+r,
+					q+r,
+					y=g[u+o+1],
+					x=1-(z-y)/r,
+					(y-=3*Math.random())>9? b(0,0,x):
+						z>6.1? b(7/y,7/y*x,x/3):
+									z>e? b(7/y,7/y*x,x):
+										b(2,y,x)]
 			}
 		}
 	}
 }	
-a.fillStyle='#000';
-a.fillRect(0, 0, w=c.width=800, h=c.height=600);
+a.fillStyle=b(),a.fillRect(0, 0, r=c.width=c.height=800),
 
-var im = [], imOr=landscapes[8],yaw = 0.51, pitch = 0, roll = -0.34,p1=[-5, -5, 20];
-for (i = 0; i < imOr.length; i++) {
-	im[i] = [];
-	for (j = 0; j < 9; j+=3) {
+q = [], h = .4, g=d[8],p=131072, r/=2, o = .8
+for (w = p; w--;) {
+	q[w] = []
+	for (j = 9; j--;
+		v=g[w][j--]-15, u=g[w][j--]+8, t=g[w][j],
+		q[w].push(z = Math.cos(o)*v - Math.sin(o)*(k=Math.cos(h)*u+Math.sin(h)*t),(Math.cos(h)*t-Math.sin(h)*u)/z*r + r, (Math.sin(o)*v + Math.cos(o)*k)/z*r + r));
+	q[w][9] = g[w][10]
+}
+q.sort(function(h,s,l){return h[3]-s[3]})
 		
-		var x=imOr[i],
-			csz = Math.sin(roll)*(x[u=j+1]-p1[1]) + Math.cos(roll)*(x[j]-p1[0]),
-			csOy = Math.cos(pitch)*(x[v=j+2]-p1[2])+Math.sin(pitch)*csz,
-			csOz = Math.cos(roll)*(x[u]-p1[1])-Math.sin(roll)*(x[j]-p1[0]),
-			coord = [Math.cos(pitch)*csz - Math.sin(pitch)*(x[v]-p1[2]),
-				Math.sin(yaw)*csOy + Math.cos(yaw)*csOz,
-				Math.cos(yaw)*csOy - Math.sin(yaw)*csOz],
-			inv = 1/coord[2];
-		im[i].push(coord[0]*inv*w/2 + w/2, coord[1]*inv*h/2 + h/2, coord[2]);
-	}
-	im[i][9] = x[10];
-}
-im.sort(function(h,s,l){return s[2]-h[2]});
+for (w=p;w--;
+	a.fillStyle= /*a.strokeStyle = */q[w][9],
+	a.beginPath(),
+	a.moveTo(q[w][1], q[w][2]),
+	a.lineTo(q[w][4], q[w][5]),
+	a.lineTo(q[w][7], q[w][8]),
+	a.fill()/*,a.stroke()*/);
 
-for (i=0 ; i < im.length ; i++) {
-	t=im[i];
-	a.fillStyle = a.strokeStyle = t[9];
-	a.beginPath();
-	a.moveTo(t[0], t[1]);
-	a.lineTo(t[3], t[4]);
-	a.lineTo(t[6], t[7]);
-	a.lineTo(t[0], t[1]);
-	a.stroke();
-	a.fill();
-}
 		</script>
 	</body>
 </html>
